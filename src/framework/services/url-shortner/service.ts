2b7f8d440e1b42e7487e5c82ce41f960ab7ef4d9
kareem paes <contact@kareempaes.com>
2023-11-22 15:31:20 -0700

    add nanoid lib

--- src/framework/services/url-shortner/service.ts
+++ src/framework/services/url-shortner/service.ts
@@ -1,9 +1,12 @@
+import { configService } from 'src/configuration/database';
 import { URLShortnerService } from './abstract';
-
+import { nanoid } from 'nanoid';
 export class URLShortnerServiceImpl implements URLShortnerService {
   constructor() {}
 
   async shortenURL(url: string): Promise<string> {
+    const id = nanoid();
+    const shortenedURL = `${configService.getValue('SHORT_URL_BASE')}/${id}`;
     return url;
   }
 }

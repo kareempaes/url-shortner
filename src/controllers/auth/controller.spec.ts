4c3984da47d715027b2adda9da2538a83cab2134
kareem paes <contact@kareempaes.com>
2023-11-22 19:42:50 -0700

    add mock for authusecase

--- src/controllers/auth/controller.spec.ts
+++ src/controllers/auth/controller.spec.ts
@@ -1,13 +1,27 @@
 import { AuthController } from './controller';
 import { AuthUseCase } from 'src/framework/use-cases/auth';
+
+const mockAuthUseCase = jest.fn<AuthUseCase, []>(() => ({
+  login: jest.fn(),
+  logout: jest.fn(),
+  register: jest.fn(),
+}));
+
+jest.mock('src/framework/use-cases/auth', () => ({
+  AuthUseCase: mockAuthUseCase,
+}));
+
 describe('Authentication Controller', () => {
   let controller: AuthController;
   let authUsecase: AuthUseCase;
 
-  beforeAll(() => {});
+  beforeAll(() => {
+    authUsecase = new mockAuthUseCase();
+    controller = new AuthController(authUsecase);
+  });
 
   beforeEach(() => {
-    controller = new AuthController(authUsecase);
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
